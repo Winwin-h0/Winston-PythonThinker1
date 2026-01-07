56cb9657a467062dce33ba31191f74f47bcfa2ab
Winwin-h0 <winstonckho@gmail.com>
2026-01-07 09:14:13 +0000

    Week 1

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -5,3 +5,105 @@ print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
+
+# Lesson 2 - Introduction to Pseudocode
+
+## Recap 1: Colours of the Rainbow
+# Task: 
+# 1. Using a new line for each colour, print out the 7
+#    colours of the rainbow.
+# 2. Then, comment out every other line except for your
+#    favourite colour.
+# print('red')
+# print("orange")
+# print("yellow")
+print("green")
+# print("blue")
+# print("indigo")
+# print("violet")
+
+
+# --------------------------------------------------------
+
+## Task 1: Solve the problem in pseudocode
+# **Task 1a**:
+# Task: Look at the screen and translate the code blocks
+# into pseudocode
+
+#say hey 1 time
+# step forward 10 times
+# repeat 10 times
+
+# repeat 10 times:
+#   say "hey!"
+#   step forward 10 times
+
+
+# **Task 1b**:
+# Task: Look at the screen and translate the code blocks
+# into pseudocode
+
+# set variable "counter" to 0
+# while variable "counter" is not 50
+#   step forward 10 times
+#   turn 15 degrees
+#   add to variable "counter" by 10
+
+# **Task 1c**:
+# Task: Look at the screen and translate the code blocks
+# into pseudocode
+
+# ask "What is your age" and assign to "answer"
+# if answer is less than 18 then
+#   say "access denied"
+# if answer is over 18 then
+#   say "Welcome"
+
+# --------------------------------------------------------
+
+# ## Task 2: Recycling Logic
+# Task: Write down the steps on how to solve the problem below
+
+# Design pseudocode for a recycling robot that sorts items
+# into bins for glass, plastic, and paper. The robot should
+# check each item's material and place it in the correct bin.
+
+# if item's material is glass then
+#   put it in glass bin
+# if item"s material is plastic then
+#   put it in plastic bin
+# if item's material is paper then
+#   put it in paper bin
+
+
+# --------------------------------------------------------
+
+# ## Task 3: What's the password?
+# Task: Using pseudocode, design a program that will check if the
+# user has entered the right secret phrase.
+
+# 1. If the user has entered a wrong secret phrase, send them an
+# error message.
+# 2. If the user has entered the right secret phrase, congratulate
+# them.
+
+# set "answer" to "secretlolipop"
+# ask "what's the password" and assign to "User input"
+# if "user input" is not the same as "answer" then
+#   say "access denied"
+# if "user input" is the same as"answer" then
+#   say "welcome"
+# --------------------------------------------------------
+
+# ## Task 4: What's your grade?
+# Task: Write a pseudocode that calculates the final score of a student
+# based on the following weightage:
+# _each test is scored upon 100_
+# Test 1 = 20%
+# Test 2 = 40%
+# Test 3 = 40%
+
+# set "test 1" to 20% of test 1
+# set "test 2" to 40% of test 2
+# set "test 3" to 40% of test 3
+# student final score = test 1 + test 2 + test 3
