d3650a7039a88ae3e543941d764759cdaa52091f
Winwin-h0 <winstonckho@gmail.com>
2026-02-16 05:03:20 +0000

    lesson 7 extra

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -216,10 +216,10 @@ print(total)
 
 # " " * (n - i) + "*" * (1 + 2 * (i - 1)) + " " * (n - i)
 
-num = int(input("Give me 'n': " ))
+# num = int(input("Give me 'n': " ))
             
-for i in range(1, (num + 1)):
-    print(" " * (num - i) + "*" * (1 + 2 * (i - 1)) + " " * (num - i))
+# for i in range(1, (num + 1)):
+#     print(" " * (num - i) + "*" * (1 + 2 * (i - 1)) + " " * (num - i))
 
 
 # 1) Centre-Aligned Number Pyramid (Wrap at 9)
@@ -242,6 +242,16 @@ for i in range(1, (num + 1)):
 # Rules:
 # - No if / else
 # - Nested for loops allowed
+# num = 1
+
+n = int(input("give me a number "))
+for i in range(1, (int(n) + 1)):
+    int(i)
+    for i in range(n):
+        print(" " * (n - i) ,num)
+        
+    num = num + 1
+    int(num)
 
 
 
