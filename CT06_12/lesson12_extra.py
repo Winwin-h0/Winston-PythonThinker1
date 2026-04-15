b74514c1cb5feb7fde858d2cae9979411df1ab81
Winwin-h0 <winstonckho@gmail.com>
2026-04-15 09:05:58 +0000

    lesson 13

--- CT06_12/lesson12_extra.py
+++ CT06_12/lesson12_extra.py
@@ -2,11 +2,19 @@
 # A1. Count from 1 to 5
 # Write a program that prints the numbers 1 to 5 using a while loop.
 # Prints 1, 2, 3, 4, 5 (each on a new line)
+counter = 1
+while counter <= 5:
 
-# A2. Count from 5 down to 1
-# Write a while loop that prints 5, 4, 3, 2, 1.
-# Prints numbers in descending order from 5 to 1
+    print(counter)
+    counter += 1
 
+# A2. Count from 10 down to 1
+# Write a while loop that prints 10, 7 ,4, 1
+# Prints numbers in descending order from 10 to 1
+counter = 10
+while counter >= 1:
+    print(counter)
+    counter -= 3
 
 # A3
 # Question:
@@ -33,50 +41,6 @@
 # Output:
 # 23
 
-## while condition and break
-
-# B1. Stop counting when you reach 3
-# Start from 1 and count upward, but use break to stop the loop when the number becomes 3.
-# Prints 1, 2, 3 and stops using break
-
-# B2. Ask until the user types "ok"
-# Use while True to keep asking for input until the user types "ok".
-# Loop repeats until "ok" is entered, then stops
-
-# B3
-# Question:
-# Write a program that asks the user to enter a number.
-# Keep looping while the number is not 99.
-# Inside the loop:
-# - if the number is less than 0 OR greater than 50, print "Invalid" and break
-# - otherwise print "Try again" and ask again
-#
-# Output:
-# If user types:
-# 10
-# 20
-# 60
-#
-# Output should be:
-# Try again
-# Try again
-# Invalid
-
-# B4
-# Question:
-# Write a program that checks the list:
-# [5, 9, 12, 15, 18, 21]
-# using a while loop and an index.
-# Keep looping while the index is still inside the list.
-# Inside the loop:
-# - if the current number is divisible by 4 OR divisible by 7, print "Found special number" and break
-# - otherwise print the number
-# - move to the next index
-#
-# Output:
-# 5
-# 9
-# Found special number
 
 
 # while True
@@ -84,10 +48,32 @@
 # C1. Password checker
 # Keep asking the user to type the password "python123". Stop only when it is correct.
 # Loop continues until correct password is entered
+password = "python123"
+# user = input("What's the password? ")
+# while user != password:
+#     print("wrong")
+#     user = input("What's the password? ")
+# else:
+#     print("Correct")
+# user = input("What's the password? ")
+while True:
+    # input is the first thing in while True
+    user = input("What's the password? ")
+    # next identify the exit condition
+    if user == password:
+        print("Correct")
+        break
 
+
+    
 # C2. Menu exit
 # Ask the user to type "play" or "quit". If they type "quit", stop the program.
 # Loop only ends when "quit" is entered
+
+while True:
+    user = input("play or quit")
+    if user == "quit":
+        break
 
 # C3
 # Question:
